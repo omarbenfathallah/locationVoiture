21aaecdd82ebc38e39503023e21eddb59460ba84
Omar Ben Fathallah <omarbenfathallah782@gmail.com>
2023-12-04 22:18:37 +0100

    add VoitureContoller

---
+++ src/main/java/com/location/locationvoiture/Controller/VoitureController.java
@@ -0,0 +1,25 @@
+package com.location.locationvoiture.Controller;
+
+import com.location.locationvoiture.Entity.Voiture;
+import com.location.locationvoiture.Service.IVoitureService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/voiture")
+public class VoitureController {
+
+    @Autowired
+    private IVoitureService voitureService;
+
+    @GetMapping("/voituresParMarque")
+    public List<Voiture> getVoituresParMarque(@RequestParam String nomMarque) {
+        return voitureService.getVoituresParMarque(nomMarque);
+    }
+}
